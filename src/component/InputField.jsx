63c07daca0734484e2bdf3987733551e118c6ebd
Yoos99 <yoosh2520@naver.com>
2024-09-02 10:49:17 +0900

    feat: 검색창 생성

---
+++ src/component/InputField.jsx
@@ -0,0 +1,46 @@
+import '../css/InputField.css';
+import { useState } from 'react';
+
+export default function InputField({ setList }) {
+  const [inputText, setInputText] = useState('');
+  const inputItem = (e) => {
+    setInputText(e.target.value);
+  };
+  const addItem = () => {
+    //공백제거, 글자수 0이면 추가 x , 경고 메시지
+    if (inputText.trim().length < 2) {
+      alert(
+        inputText.trim === ''
+          ? '내용을 입력해주세요.'
+          : '2글자 이상 입력해주세요.'
+      );
+      document.querySelector('tripInput').focus();
+      return;
+    }
+    setList((prev) => [inputText, ...prev]);
+    setInputText('');
+    document.querySelector('tripInput').focus();
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      addItem();
+    }
+  };
+
+  return (
+    <div className="IninputField mw">
+      <input
+        value={inputText}
+        type="text"
+        placeholder="여행지를 입력해주세요."
+        onChange={inputItem}
+        onKeyUp={handleKeyUp}
+      />
+      <button onClick={addItem}>
+        검색
+        {/* <i className="fa-solid fa-plus"></i> */}
+      </button>
+    </div>
+  );
+}
