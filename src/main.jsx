cf1c7ad685ea5c18087852b7db70a49320a95885
Yoos99 <yoosh2520@naver.com>
2024-09-02 12:34:37 +0900

    Fix: vite BrowserRouter 해결 및 config '{ }' 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom/BrowserRouter';
+// import { BrowserRouter } from 'react-router-dom/BrowserRouter';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  // <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
+  <StrictMode>
+    <App />
+  </StrictMode>
+  // </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/{todo-travellist}/', // {repo-name}을 GitHub 저장소 이름으로 변경
+  base: '/todo-travellist/', // {repo-name}을 GitHub 저장소 이름으로 변경
 });
